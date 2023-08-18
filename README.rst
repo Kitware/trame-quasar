cc59e7e0ae3e54ea3ce89e83e977020cf776b568
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-08-17 22:14:59 -0600

    fix(package): finish package setup

---
+++ README.rst
@@ -0,0 +1,24 @@
+Quasar widget for trame
+===========================================================================
+
+Trame bundle for the Quasar Vue library.
+
+-----------------------------------------------------------
+
+trame-quasar is made available under the MIT License. For more details, see `LICENSE <https://github.com/Kitware/trame-quasar/blob/master/LICENSE>`_
+This license has been chosen to match the one use by `Quasar <https://github.com/quasarframework/quasar/blob/dev/LICENSE>`_ which is the core of that library possible.
+
+
+Community
+-----------------------------------------------------------
+
+`Trame <https://kitware.github.io/trame/>`_ | `Discussions <https://github.com/Kitware/trame/discussions>`_ | `Issues <https://github.com/Kitware/trame/issues>`_ | `RoadMap <https://github.com/Kitware/trame/projects/1>`_ | `Contact Us <https://www.kitware.com/contact-us/>`_
+
+.. image:: https://zenodo.org/badge/410108340.svg
+    :target: https://zenodo.org/badge/latestdoi/410108340
+
+
+Enjoying trame?
+-----------------------------------------------------------
+
+Share your experience `with a testimonial <https://github.com/Kitware/trame/issues/18>`_ or `with a brand approval <https://github.com/Kitware/trame/issues/19>`_.
---
+++ example/layout.py
@@ -0,0 +1,77 @@
+from trame.app import get_server
+from trame.ui.html import DivLayout
+from trame.widgets import quasar, html
+
+server = get_server()
+server.client_type = "vue3"
+server.state.dark = False
+
+with DivLayout(server):
+    with quasar.QLayout(
+        view="lhh LpR lff",
+        container=True,
+        style="width: 100vw; height: 100vh;",
+        classes=(
+            """{
+                'shadow-2': 1,
+                'rounded-borders': 1,
+                'bg-grey-9': dark,
+                'bg-grey-3': !dark,
+            }""",
+        ),
+    ):
+        with quasar.QHeader(
+            reveal=True,
+            classes=("{ 'bg-secondary': dark, 'bg-black': !dark}",),
+        ):
+            with quasar.QToolbar():
+                quasar.QBtn(
+                    flat=True,
+                    click="drawerLeft = !drawerLeft",
+                    round=True,
+                    dense=True,
+                    icon="menu",
+                )
+                quasar.QToolbarTitle("Header")
+                quasar.QBtn(
+                    flat=True,
+                    click="drawerRight = !drawerRight",
+                    round=True,
+                    dense=True,
+                    icon="menu",
+                )
+
+        with quasar.QFooter():
+            with quasar.QToolbar():
+                quasar.QToolbarTitle("Footer")
+
+        with quasar.QDrawer(
+            v_model=("drawerLeft", False),
+            width=(200,),
+            breakpoint=(700,),
+            bordered=True,
+        ):
+            with quasar.QScrollArea(classes="fit"):
+                with html.Div(classes="q-pa-sm"):
+                    html.Div("Drawer {{ n }} / 50", v_for="n in 50", key="n")
+
+        with quasar.QDrawer(
+            side="right",
+            v_model=("drawerRight", False),
+            width=(200,),
+            breakpoint=(500,),
+            bordered=True,
+        ):
+            with quasar.QScrollArea(classes="fit"):
+                with html.Div(classes="q-pa-sm"):
+                    html.Div("Drawer {{ n }} / 50", v_for="n in 50", key="n")
+
+        with quasar.QPageContainer():
+            with quasar.QPage(style="padding-top: 60px", classes="q-pa-md"):
+                with html.P(v_for="n in 15", key="n") as c:
+                    c.add_child(
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?"
+                    )
+
+
+server.start()
